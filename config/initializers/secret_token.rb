50f8193282f06ad4fee4763ef73f8b86dd27f53c
victorhazbunanuff <victor.hazbun@koombea.com>
2012-06-25 21:41:10 -0500

    Create Rails -v 3.2.2 application - Ruby -v 1.9.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inventory::Application.config.secret_token = '0076bcb2a1702940bb903c4d3893836d76d651e4c5ece87a553691b340f3343770fba173240b73e21d2a7e37c019a5d05b49d1f0b89b7e63646cc894f07f6f6f'
